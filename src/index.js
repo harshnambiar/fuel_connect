6ad38ce24e207b99c99acbeedc40e124463420c6
harshnambiar <harrisonlvl2@gmail.com>
2024-07-05 15:34:19 +0530

    added working demo of signed transactions

--- src/index.js
+++ src/index.js
@@ -24,6 +24,16 @@ async function connect(code) {
     const wallet = Wallet.fromAddress(accounts[0].toString());
     const evmWallet = wallet.address.toB256();
     console.log(evmWallet);
+    const account = await fuel.currentAccount();
+    if (!account) {
+      throw new Error("Current account not authorized for this connection!");
+    }
+    const wallet1 = await fuel.getWallet(account);
+    const amount = 1;
+    const destination = '0x19a0cef7d3e389890590bd41ddca75e834a267a15a8ea5ac03f4e45006378200';
+    const transactionRequest = await wallet1.createTransfer(destination, amount);
+    const transactionId = await fuel.sendTransaction(account, transactionRequest);
+    console.log("Transaction ID", transactionId);
 
 
 
